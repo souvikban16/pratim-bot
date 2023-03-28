c8390fac1901393186b73a32e3f28df9d3405a93
souvikban16 <59077658+souvikban16@users.noreply.github.com>
2023-03-28 19:48:39 +0530

    added into flask app for uptime

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
+from bot import bot
+
 import discord
 import re
 
-intents = discord.Intents.default()
-intents.members = True
 
-client = discord.Client(intents=intents)
+class MyClient(discord.Client):
 
-@client.event
-async def on_ready():
+  #@client.event
+  async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
+  #@client.event
+  async def on_message(message):
     print("comes here")
     print(message.author.name)
     if message.author == client.user:
-        return # to prevent bot form kicking itself
+      return  # to prevent bot form kicking itself
     evil_user = "navoneel"
     print(type(message.author.name))
     if re.search(evil_user, message.author.name):
-        await message.author.guild.kick(message.author)
-        await message.channel.send(f'navoneel can fuck off!')
+      await message.author.guild.kick(message.author)
+      await message.channel.send(f'navoneel can fuck off!')
 
-        
 
-client.run(<bot_token>)
+intents = discord.Intents.default()
+intents.message_content = True
+bot()
+
+client = MyClient(intents=intents)
+client.run(
+  '')
